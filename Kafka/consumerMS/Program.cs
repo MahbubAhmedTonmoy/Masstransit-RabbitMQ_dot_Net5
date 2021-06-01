5fe7ed5b27d51a3c088e21d3b7eb5ff933da21cc
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2021-06-01 14:58:21 +0600

    kafka

---
+++ Kafka/consumerMS/Program.cs
@@ -0,0 +1,28 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace consumerMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "gid-consumers",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe("abc");
+                while (true)
+                {
+                    var cr = consumer.Consume();
+                    Console.WriteLine(cr.Message.Value);
+                }
+            }
+        }
+    }
+}
